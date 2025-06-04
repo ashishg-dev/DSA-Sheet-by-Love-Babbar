e96916650f12750f83b8b5b4af98755d17e2b7e3
Ashish Gupta <ashishgupta058@gmail.com>
2025-06-04 12:53:45 +0530

    Update and rename 29-Segregate-even-and-odd-nodes-LL.java to 29_Segregate_even_and_odd_nodes_LL.java

--- linked-list/29_Segregate_even_and_odd_nodes_LL.java
+++ linked-list/29_Segregate_even_and_odd_nodes_LL.java
@@ -1,5 +1,6 @@
 /**
-Given a link list, modify the list such that all the even numbers appear before all the odd numbers in the modified list. The order of appearance of numbers within each segregation should be the same as that in the original list.
+Given a link list, modify the list such that all the even numbers appear before all the odd numbers in the modified list.
+The order of appearance of numbers within each segregation should be the same as that in the original list.
 
 NOTE: Don't create a new linked list, instead rearrange the provided one.
 
